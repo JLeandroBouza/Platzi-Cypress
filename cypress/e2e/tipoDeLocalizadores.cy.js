8b905bb26ba1d3b8f695756312ea66e47d3bf6b3
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-06-12 21:33:46 -0300

    Tratando de corregir los Fetch

--- cypress/e2e/tipoDeLocalizadores.cy.js
+++ cypress/e2e/tipoDeLocalizadores.cy.js
@@ -1,32 +1,41 @@
+
+
 describe('Tipo de Localizadores', () => {
+  
 
     it('Obtenerlo por medio de un TAG', () => {
+      cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
       cy.visit('/automation-practice-form')
       cy.get('input')
     
     })
 
     it('Obtenerlo por medio de un atributo', () => {
+      cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
       cy.visit('/automation-practice-form')
       cy.get('[placeholder="First Name"]')
     })
 
     it('Obtenerlo por medio de un atributo y un tag', () => {
+      cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
       cy.visit('/automation-practice-form')
       cy.get('input[placeholder="First Name"]')
     })
 
     it('Obtenerlo por medio de un ID', () => {
+      cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
       cy.visit('/automation-practice-form')
       cy.get('#firstName')
     })
 
     it('Obtenerlo por medio de una clase', () => {
+      cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
       cy.visit('/automation-practice-form')
       cy.get('.mr-sm-2.form-control') // son dos clases por eso se usa el punto para separarlas.
     })
 
     it('Usando contains', () => {
+      cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
       cy.visit('/automation-practice-form')
       cy.contains('Reading')
       cy.contains('.header-wrapper','Widgets')
