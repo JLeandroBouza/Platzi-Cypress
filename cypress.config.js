e9a1035db594be0e6fb52770a5e009b6cfcf6165
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-06-15 21:20:23 -0300

    Debugger

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+
+      on('task',{
+        log(message){
+         console.log('Mensaje del console log del task ' + message)
+         return null
+        } 
+     })
     
       // implement node event listeners here
     },
   baseUrl: 'https://demoqa.com'
   },
+ 
 });
