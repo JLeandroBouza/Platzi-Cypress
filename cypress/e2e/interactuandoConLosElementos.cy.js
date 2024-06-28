35dbecc2565df52fa167463b05b234943d6529ea
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-06-27 22:56:54 -0300

    Interactuando con elementos: Dropbox, selectores, tablas

--- cypress/e2e/interactuandoConLosElementos.cy.js
+++ cypress/e2e/interactuandoConLosElementos.cy.js
@@ -55,4 +55,83 @@ describe('Interactuar con los elementos', () => {
            
     })
 
+    it ('Extrayendo info', ()=>{
+        cy.visit('/automation-practice-form')
+        cy.get('#firstName').as('nombre')
+        cy.get('@nombre').type('Leandro')
+
+        cy.get('@nombre').then (($nombre)=>{
+
+            expect($nombre.val()).to.equal('Leandro')
+        })
     })
+
+    
+    it ('Compartir info', ()=>{
+        cy.visit('/automation-practice-form')
+        cy.get('#firstName').as('nombre')
+        cy.get('@nombre').type('Leandro')
+    
+        cy.get('@nombre').then (($nombre)=>{
+    
+            expect($nombre.val()).to.equal('Leandro')
+        })
+           
+    })
+
+
+    it ('Interactuando con los dropdown(select)', ()=>{
+        cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+        cy.get('.custom-select').select(10)
+        cy.get('.custom-select').select('3').should('have.value', '3')
+        cy.get('.custom-select').select('Greece').should('have.value', '4')
+    })
+
+    it ('Interactuando con los dropdown(select) dinamico', ()=>{
+        cy.visit('https://react-select.com/home')
+        cy.get('#react-select-6-input').type(' ')
+        cy.get('#react-select-6-listbox').children().children().each(($el,index,$list)=>{
+
+            if ($el.text()=== 'Red'){
+                $el.on('click')
+                //$el.click()
+            }
+
+        })
+
+        cy.get('#react-select-6-3').click()
+    })
+
+    it ('Interactuando con tablas', ()=>{
+        cy.visit('https://www.w3schools.com/html/html_tables.asp')
+        cy.get('#customers')
+            .find('th')
+            .each(($el)=>{
+                cy.log($el.text())
+            })
+        
+        cy.get('#customers')
+            .find('th')
+            .first()
+            .invoke('text')
+            .should('equal', 'Company')
+        
+        cy.get('#customers')
+            .find('th')
+            .eq(1)
+            .invoke('text')
+            .should('equal', 'Contact')
+        
+        cy.get('#customers')
+            .find('th')
+            .eq(2)
+            .invoke('text')
+            .should('equal', 'Country')
+        
+        cy.get('#customers').find('tr').should('have.length', 7)
+
+        
+    })
+
+
+})
